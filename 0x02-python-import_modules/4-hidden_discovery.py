ab22336a1f12679d384779cf364514b434947954
Tuski2304 <otuchituru.anyahara@gmail.com>
2023-01-25 10:24:54 +0000

    Who are you

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+i = 0
+
+if __name__ == '__main__':
+    mylist = dir(hidden_4)
+    newlist = sorted(mylist)
+    while i < len(newlist):
+        if newlist[i][0] != '_':
+            print(newlist[i])
+        i += 1
